8baaf521c4d1e671664834577f59b72d172c6ce9
Fahadbashir64 <101262326+Fahadbashir64@users.noreply.github.com>
2022-03-14 15:35:42 +0500

    updated port

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
 });
 app.use(express.static(path.join(__dirname, "/public/build")));
 
-const PORT = process.env.PORT ?? 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 //app.listen(5000, () => console.log("Listening on port 5000"));
