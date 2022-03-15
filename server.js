dae782f76c673a1dc80df042c59d3b240e886bd2
Fahadbashir64 <101262326+Fahadbashir64@users.noreply.github.com>
2022-03-15 19:30:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(cors());
     })
     .catch((err) => console.log(err));
 });
-
+*/
+var token;
 app.post("/", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   console.log("hello2");
@@ -44,6 +45,7 @@ app.post("/", (req, res) => {
     }
       .then(function (res) {
         console.log("post");
+        token = res;
         console.log(res);
       })
       .catch(function (res) {
@@ -52,7 +54,7 @@ app.post("/", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/build/index.html"));
 });
 app.use(express.static(path.join(__dirname, "/public/build")));*/
@@ -79,11 +81,13 @@ function groupBy(key, array) {
 }
 
 app.get("/", (req, res) => {
-  fetch("https://api.prepaidforge.com/v1/1.0/findAllProducts", {
+  fetch("https://api.prepaidforge.com/v1/1.0/findStocks", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      "X-PrepaidForge-Api-Token": `${token}`,
     },
+    data: { types: ["TEXT", "SCAN"], skus: ["Netflix-25-Eur"] },
     /*body: JSON.stringify({
       // your expected POST request payload goes here
       email: "Worldofprodiverse@gmail.com",
@@ -93,9 +97,9 @@ app.get("/", (req, res) => {
     .then((res) => res.json())
     .then((data) => {
       // enter you logic when the fetch is successful
-      var stocks = groupBy("skus", data);
-      console.log(stocks);
-      res.send(stocks);
+      // var stocks = groupBy("skus", data);
+      console.log(data);
+      res.send(data);
     })
     .catch((error) => {
       // enter your logic for when there is an error (ex. error toast)
