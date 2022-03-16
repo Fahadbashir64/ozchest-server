28bced679562678fceb7e68d100cc2e653983eea
Fahadbashir64 <101262326+Fahadbashir64@users.noreply.github.com>
2022-03-16 19:18:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,20 @@ import fetch from "node-fetch";
 import path from "path";
 app.use(cors());
 
-/*app.get("/signin", (req, res) => {
+app.get("/signin", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   console.log("hello1");
-  fetch("https://api.prepaidforge.com/v1/1.0/findAllProducts", {
+
+  fetch("https://api.prepaidforge.com/v1/1.0/findProductPage", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET,POST,OPTIONS,DELETE,PUT",
     },
-    method: "GET",
+    body: JSON.stringify({
+      // your expected POST request payload goes here
+      page: 369,
+      pageSize: 10,
+    }),
   })
     .then((res) => res.json())
     .then((data) => {
@@ -25,7 +28,7 @@ app.use(cors());
       res.send(data);
     })
     .catch((err) => console.log(err));
-});*/
+});
 
 var token;
 
@@ -79,7 +82,7 @@ function groupBy(key, array) {
   return result;
 }
 
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   console.log(token);
   fetch("https://api.prepaidforge.com/v1/1.0/signInWithApi", {
     method: "POST",
@@ -113,7 +116,7 @@ app.get("/", (req, res) => {
       // your expected POST request payload goes here
       email: "Worldofprodiverse@gmail.com",
       password: "Bravo1?@1",
-    }),*/
+    }),
       })
         .then((response) => response.json())
         .then((data1) => {
@@ -131,7 +134,7 @@ app.get("/", (req, res) => {
       // enter your logic for when there is an error (ex. error toast)
       console.log(error);
     });
-});
+});*/
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
