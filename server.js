95760c3808cc76e9551f9e71b67b10e968c856b6
Fahadbashir64 <101262326+Fahadbashir64@users.noreply.github.com>
2022-03-19 16:20:31 +0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
     body: JSON.stringify({
       // your expected POST request payload goes here
       page: 555,
-      pageSize: 30,
+      pageSize: 10,
     }),
   })
     .then((response) => response.json())
