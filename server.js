be9041d37a1ac71579c9f616ce7730e463dd8a3b
Fahadbashir64 <101262326+Fahadbashir64@users.noreply.github.com>
2022-04-08 15:09:11 +0500

    Update server.js

--- server.js
+++ server.js
@@ -188,11 +188,13 @@ app.post("/", (req, res) => {
       currencyCode: req.body.code,
       faceValue: req.body.price,
     }).then((res2) => {
+      res.send(res2);
       fetch("https://api.prepaidforge.com/v1/1.0/findStocks", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "X-PrepaidForge-Api-Token": req.body.apitoken,
+          "X-PrepaidForge-Api-Token":
+            "d1eafd1903776da624c47aee6c6e00a1e7b9adda129dd484742599df6675b331",
         },
 
         body: JSON.stringify({
@@ -317,7 +319,7 @@ app.get("/", (req, res) => {
       // var stocks = groupBy("skus", data);
       console.log("hello");
       console.log(data.apiToken);
-      res.send(data);
+      //res.send(data);
     })
     .catch((error) => {
       // enter your logic for when there is an error (ex. error toast)
